2d0e1c4dae82c4c00818606dfd1217a6ca17f56a
aburston <aburston@juniper.net>
2026-04-16 18:16:11 +0530

    fix: black formatting + eslint config for super-linter
    - Run black on all Python files (docstring spacing)
    - Add FILTER_REGEX_EXCLUDE to skip vendored JS (jQuery, SlickGrid, etc.)
    - Add .github/linters/.eslintrc.json with browser/jQuery env
    - Set JAVASCRIPT_ES_CONFIG_FILE for super-linter

--- build-and-test-webapp/add_jenkins_job.py
+++ build-and-test-webapp/add_jenkins_job.py
@@ -1,4 +1,4 @@
-""" ********************************************************
+"""********************************************************
 
 Project: nita-webapp
 
@@ -10,7 +10,7 @@ SPDX-License-Identifier: Apache-2.0
 
 Third-Party Code: This code may depend on other components under separate copyright notice and license terms. Your use of the source code for those components is subject to the terms and conditions of the respective license as noted in the Third-Party source code file.
 
-******************************************************** """
+********************************************************"""
 
 import os
 import jenkins
